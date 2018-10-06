55f6cc25364c7570a3b30426c202b74ba59da1b1
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-07 00:38:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('zBSXkKQNXKTM8jbLTLO2KxyeYJKO8J8KG5ONungzu1IDzv5GA3bT0DznUYbgEvwPIqTT3GyvWNypRQ0dZR0Rxd3ehn/vEu3uDIKU5W2V6WioPpcJYYHmjIthKyyIQb5t3LbPXeNVWqlFh1CihpHw/AdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('4amhqNoNxD6PIejZgRDjA0RokbkkOss9A6KRiyeuQOTSBbGOYaNrLotpaPmQ9KKAIqTT3GyvWNypRQ0dZR0Rxd3ehn/vEu3uDIKU5W2V6WiWoxdrDw04HcRh0y7LU2fOdjmXJoC4Z9atZFo2i6D/xgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e74028b8fa4f4920f6c20f899359b4c9')
 
 @app.route("/")
