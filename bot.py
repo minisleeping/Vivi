43501cc025b88c5ddfce198e33015b1ddc3df6dd
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-07 00:57:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,6 @@
 from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi('4amhqNoNxD6PIejZgRDjA0RokbkkOss9A6KRiyeuQOTSBbGOYaNrLotpaPmQ9KKAIqTT3GyvWNypRQ0dZR0Rxd3ehn/vEu3uDIKU5W2V6WiWoxdrDw04HcRh0y7LU2fOdjmXJoC4Z9atZFo2i6D/xgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('e74028b8fa4f4920f6c20f899359b4c9')
 
 @app.route("/")
 def hello():
@@ -14,28 +8,8 @@ def hello():
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
+if request.method == 'POST':
     return 'OK'
     
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
 if __name__ == "__main__":
     app.run()
