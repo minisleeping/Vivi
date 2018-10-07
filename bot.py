a0d1f6215aa484df1421c043e053338886622c94
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 04:10:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def find_inExcel(Value):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    soruser = event.source
+    soruser = dict(event.source)
     print (soruser)
     print (soruser["userId"])
     if find_inExcel(event.message.text) != None:
