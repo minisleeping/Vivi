e552c676ba954ba686849207b2cb97d680ebac4e
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 01:24:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
-
+import xlrd 
+sheet = ''
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('4amhqNoNxD6PIejZgRDjA0RokbkkOss9A6KRiyeuQOTSBbGOYaNrLotpaPmQ9KKAIqTT3GyvWNypRQ0dZR0Rxd3ehn/vEu3uDIKU5W2V6WiWoxdrDw04HcRh0y7LU2fOdjmXJoC4Z9atZFo2i6D/xgdB04t89/1O/w1cDnyilFU=')
@@ -28,14 +29,27 @@ def webhook():
         abort(400)
 
     return 'OK'
-    
+
+def open_file(path):
+    global sheet
+    wb = xlrd.open_workbook(path)
+    sheet = wb.sheet_by_index(0)
+
+def find_inExcel(Value):
+    global sheet
+    for row_num in range(sheet.nrows):
+        row_value = sheet.row_values(row_num)
+        if row_value[2] == Value:
+            return row_value
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=str(find_inExcel(event.message.text))))
 
 
 if __name__ == "__main__":
+    path = "testdatabase.xlsx"
+    open_file(path)
     app.run()
