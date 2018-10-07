7c9504ddbcfd80ba12a621cbf0c0f495ed7d8843
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 01:44:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,9 +41,10 @@ def find_inExcel(Value):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print find_inExcel(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=find_inExcel(event.message.text)))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
