e0edb1ccd44e58b421ac6dab41e80978fc0c6f3d
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 02:31:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ def find_inExcel(Value):
 def handle_message(event):
     if find_inExcel(event.message.text) != None:
         row_value = find_inExcel(event.message.text)
-        backtext = str(row_value[2]) + ' ราคา = ' + str(row_value[4]) + ' บาท') 
+        backtext = str(row_value[2]) + ' ราคา = ' + str(row_value[4]) + ' บาท'
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=backtext))
