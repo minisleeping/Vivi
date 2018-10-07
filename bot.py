9d1616a6e95887e8d01f165aec6caac1748b36a2
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 03:27:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 import xlrd 
-sheet = ''
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('4amhqNoNxD6PIejZgRDjA0RokbkkOss9A6KRiyeuQOTSBbGOYaNrLotpaPmQ9KKAIqTT3GyvWNypRQ0dZR0Rxd3ehn/vEu3uDIKU5W2V6WiWoxdrDw04HcRh0y7LU2fOdjmXJoC4Z9atZFo2i6D/xgdB04t89/1O/w1cDnyilFU=')
@@ -41,8 +41,8 @@ def find_inExcel(Value):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print (event.source)
-    print (event.source.userId)
+    profile = line_bot_api.get_profile(user_id)
+    print (profile)
     if find_inExcel(event.message.text) != None:
         row_value = find_inExcel(event.message.text)
         backtext = str(row_value[2]) + ' ราคา = ' + str(row_value[4]) + ' บาท'
