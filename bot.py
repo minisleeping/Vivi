67ba71868151a307e464e59aa256ad9eb255f04b
minisleeping <40069442+minisleeping@users.noreply.github.com>
2018-10-08 13:32:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,6 +32,7 @@ def webhook():
 
 def find_inExcel(Value, col, path):
     wb = xlrd.open_workbook(path)
+    print (Value.split())
     TextValue1,TextValue2 = Value.split()
     sheet = wb.sheet_by_index(0)
     for row_num in range(sheet.nrows):
